5663a0e7c127c41cfc7a12ec002299f67ac59f6a
Jon Biggerstaff <jonbig04@gmail.com>
2022-11-09 14:37:14 -0800

    Update contribution_data_cleaning.sql

--- data_analysis_projects/political_relational_database/contribution_data_cleaning.sql
+++ data_analysis_projects/political_relational_database/contribution_data_cleaning.sql
@@ -1,1 +1,29 @@
+/***********************************************************************/
+/*SQL Data Cleaning/Preprocessing Contribution Data Scraped from tracer*/
+/***********************************************************************/
 
+/*Creating table in MYSQL Workbench*/
+
+CREATE TABLE `colorado_project`.`contirbution_data` (
+  `committee_id` VARCHAR(45) NOT NULL,
+  `amount` DECIMAL NULL,
+  `date` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NULL,
+  `first_name` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` DECIMAL NULL,
+  `zipcode` VARCHAR(45) NULL,
+  `contribution_type` VARCHAR(45) NULL,
+  `contributor_type` VARCHAR(45) NULL,
+  `electioneering` VARCHAR(45) NULL,
+  `committee_type` VARCHAR(45) NULL,
+  `committee_name` VARCHAR(45) NULL,
+  `candidate_name` VARCHAR(45) NULL,
+  `employer` VARCHAR(45) NULL,
+  `occupation` VARCHAR(45) NULL,
+  `jurisdiction` VARCHAR(45) NULL,
+  PRIMARY KEY (`committee_id`));
+
+  
+/*****************************************/
