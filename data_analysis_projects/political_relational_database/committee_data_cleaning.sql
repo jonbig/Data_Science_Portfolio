3efae6bdcf94332efadd4a5b5b8081263dddfdf1
Jon Biggerstaff <jonbig04@gmail.com>
2022-11-09 12:09:24 -0800

    Update committee_data_cleaning.sql

--- data_analysis_projects/political_relational_database/committee_data_cleaning.sql
+++ data_analysis_projects/political_relational_database/committee_data_cleaning.sql
@@ -1,1 +1,20 @@
+/********************************************************************/
+/*SQL Data Cleaning/Preprocessing Committee Data Scraped from tracer*/
+/********************************************************************/
 
+/*Creating table in MYSQL Workbench*/
+
+CREATE TABLE `colorado_project`.`committee_data` (
+  `committee_name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `committee_id` VARCHAR(45) NOT NULL,
+  `committee_type` VARCHAR(45) NULL,
+  `committee_purpose` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `agent` VARCHAR(45) NULL,
+  `expenditures` DECIMAL NULL,
+  `contributions` DECIMAL NULL,
+  `ending_balance` DECIMAL NULL,
+  PRIMARY KEY (`committee_id`));
+  
+/*****************************************/
